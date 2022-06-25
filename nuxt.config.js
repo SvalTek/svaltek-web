28116c149f60874a677a24751a0fbe25e6c09e85
Theros <747653+Therosin@users.noreply.github.com>
2022-06-25 04:02:48 +0000

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,11 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Various Projects by SvalTek' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Various Projects by SvalTek',
+      },
       { name: 'format-detection', content: 'telephone=no' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
