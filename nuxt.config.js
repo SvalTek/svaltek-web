ac542e8a5bb0f943b7261cef123189e490ad7065
Theros <747653+Therosin@users.noreply.github.com>
2022-06-25 03:52:56 +0000

    UPDATE: set correct name, fixed Prettier ignores

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,14 +7,14 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'svaltek-web',
+    title: 'Projects @ SvalTek',
     htmlAttrs: {
       lang: 'en',
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: 'description', content: 'Various Projects by SvalTek' },
       { name: 'format-detection', content: 'telephone=no' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
